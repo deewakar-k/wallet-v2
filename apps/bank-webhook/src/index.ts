16a6aa2318968a2056454106bc8fad5d03fec638
deewakar <dakshftw01@gmail.com>
2024-11-16 11:57:51 +0530

    wehook handler

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -1,5 +1,47 @@
 import express from "express";
+import db from "@wallet/db/client";
 
 const app = express();
 
-app.post("/hdfc-webhook", (req, res) => {});
+app.post("/hdfc-webhook", async (req, res) => {
+  //TODO: add zod validation here
+
+  const paymentInfo = {
+    token: req.body.token,
+    userId: req.body.user_identifier,
+    amount: req.body.amount,
+  };
+
+  try {
+    await db.$transaction([
+      db.balance.update({
+        where: {
+          userId: paymentInfo.userId,
+        },
+        data: {
+          amount: {
+            increment: paymentInfo.amount,
+          },
+        },
+      }),
+
+      db.onRampTransaction.update({
+        where: {
+          token: paymentInfo.token,
+        },
+        data: {
+          status: "Success",
+        },
+      }),
+    ]);
+
+    res.status(200).json({
+      message: "captured",
+    });
+  } catch (e) {
+    console.error(e);
+    res.status(411).json({
+      message: "error while processing webhook",
+    });
+  }
+});
